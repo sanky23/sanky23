c3aa0e0e621f9e3b18c5c1b9d62f5a14e17f89ea
sanky23 <sanky23@users.noreply.github.com>
2021-05-24 20:10:36 -0700

    Update time_epoch_milliseconds.py

--- time_epoch_milliseconds.py
+++ time_epoch_milliseconds.py
@@ -1,10 +1,11 @@
+#Yesterday midnight to 11.59.59 whole day in miliseconds epoch generator
+
 import datetime
 def get_yesterday_epoch() -> (int, int):
     today = datetime.datetime.now()
-    yesterday1 = today.date() - datetime.timedelta(days=3)
-    yesterday = today.date() - datetime.timedelta(days=1)
 
-    yesterday_midnight = datetime.datetime.combine(yesterday1, datetime.time.min)
+    yesterday = today.date() - datetime.timedelta(days=1)
+    yesterday_midnight = datetime.datetime.combine(yesterday, datetime.time.min)
     yesterday_235959 = datetime.datetime.combine(yesterday, datetime.time.max)
     yesterday_midnight_millis = int(yesterday_midnight.timestamp() * 1000)
     yesterday_235959_millis = int(yesterday_235959.timestamp() * 1000)
